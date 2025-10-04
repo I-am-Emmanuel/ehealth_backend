db4284aa668e3b7350f185bb16739408fd6d3ada
I-am-Emmanuel <olaifaemmanueloluwatobiloba@gmail.com>
2025-10-04 16:38:58 +0100

    permission granted for all hospitals

---
+++ converter.py
@@ -0,0 +1,5 @@
+with open(r"C:\Users\user\Documents\Unilag_project\project_backend\fixtures\data.json", "r", encoding="utf-16") as f:
+    data = f.read()
+
+with open(r"C:\Users\user\Documents\Unilag_project\project_backend\fixtures\data.json", "w", encoding="utf-8") as f:
+    f.write(data)
