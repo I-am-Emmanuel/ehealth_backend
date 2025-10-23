3ac2333ae2d5863b9e6e4a0a3d67ea78511cc941
I-am-Emmanuel <olaifaemmanueloluwatobiloba@gmail.com>
2025-10-23 21:41:17 +0000

    resolved allowed hostin settings.py

--- ehealth_backend/settings.py
+++ ehealth_backend/settings.py
@@ -33,6 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 #SECURITY  don't run with debug turned on in production!
+ALLOWED_HOST = ["249074264.pythonanywhere.com"]
 
 CSRF_TRUSTED_ORIGINS = [
     # 'http://localhost:3000',
